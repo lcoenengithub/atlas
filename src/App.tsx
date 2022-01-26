23234c42a4ce070f41ce0b6300522cfb10b352f8
Klaudiusz Dembler <accounts@kdembler.com>
2022-01-26 13:26:34 +0100

    fix maintenance screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,48 @@
-import { ApolloProvider } from '@apollo/client'
+// import { ApolloProvider } from '@apollo/client'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 
-import { AnalyticsManager } from '@/AnalyticsManager'
-import { createApolloClient } from '@/api'
-import { ConfirmationModalProvider } from '@/providers/confirmationModal'
+// import { BrowserRouter } from 'react-router-dom'
+// import { AnalyticsManager } from '@/AnalyticsManager'
+// import { MainLayout } from '@/MainLayout'
+// import { createApolloClient } from '@/api'
+// import { ConfirmationModalProvider } from '@/providers/confirmationModal'
 import { GlobalStyles } from '@/styles'
 
 import { Maintenance } from './Maintenance'
-import { AssetsManager } from './providers/assets'
-import { OverlayManagerProvider } from './providers/overlayManager'
-import { Snackbars } from './providers/snackbars'
-import { StorageProvidersProvider } from './providers/storageProviders'
+
+// import { AssetsManager } from './providers/assets'
+// import { OverlayManagerProvider } from './providers/overlayManager'
+// import { Snackbars } from './providers/snackbars'
+// import { StorageProvidersProvider } from './providers/storageProviders'
 
 export const App = () => {
   // App doesn't accept props and doesn't contain state so should never rerender
-  const apolloClient = createApolloClient()
+  // const apolloClient = createApolloClient()
 
   return (
     <>
+      <Maintenance />
       <GlobalStyles />
-      <AnalyticsManager />
-      <ApolloProvider client={apolloClient}>
-        <BrowserRouter>
-          <OverlayManagerProvider>
-            <StorageProvidersProvider>
-              <ConfirmationModalProvider>
-                <Maintenance />
-                <Snackbars />
-                <AssetsManager />
-              </ConfirmationModalProvider>
-            </StorageProvidersProvider>
-          </OverlayManagerProvider>
-        </BrowserRouter>
-      </ApolloProvider>
     </>
   )
+
+  // return (
+  //   <>
+  //     <GlobalStyles />
+  //     <AnalyticsManager />
+  //     <ApolloProvider client={apolloClient}>
+  //       <BrowserRouter>
+  //         <OverlayManagerProvider>
+  //           <StorageProvidersProvider>
+  //             <ConfirmationModalProvider>
+  //               <MainLayout />
+  //               <Snackbars />
+  //               <AssetsManager />
+  //             </ConfirmationModalProvider>
+  //           </StorageProvidersProvider>
+  //         </OverlayManagerProvider>
+  //       </BrowserRouter>
+  //     </ApolloProvider>
+  //   </>
+  // )
 }
